9875ffaf7f2ede17b3a644f302b470e807bec973
Rob Hess <hessro@oregonstate.edu>
2022-01-31 15:22:25 -0800

    Add code from 1/31 lecture.

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -5,9 +5,26 @@ const userId = 1234;
 function Post() {
   const [ title, setTitle ] = useState("");
   const [ body, setBody ] = useState("");
+
+  async function sendPost() {
+    if (title && body) {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: 'POST',
+          body: JSON.stringify({ title, body, userId }),
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+      const responseBody = await res.json();
+      console.log("== responseBody:", responseBody);
+    }
+  }
+
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
+      sendPost();
     }}>
       <div>
         <input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
+import Spinner from '../components/Spinner';
+import ErrorContainer from '../components/ErrorContainer';
 
 function Search({ query }) {
   const [ inputQuery, setInputQuery ] = useState(query || "");
   const [ searchParams, setSearchParams ] = useSearchParams()
+
+  const [ repos, setRepos ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState(false);
+
+  useEffect(() => {
+    let ignore = false;
+    const controller = new AbortController();
+    async function fetchSearchResults() {
+      let responseBody = {};
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.github.com/search/repositories?q=${query}`,
+          { signal: controller.signal }
+        );
+        responseBody = await response.json();
+      } catch (e) {
+        if (e instanceof DOMException) {
+          console.log("== HTTP request cancelled")
+        } else {
+          setError(true);
+          throw e;
+        }
+      }
+      if (!ignore) {
+        setLoading(false);
+        setError(false);
+        setRepos(responseBody.items || []);
+      }
+    }
+    if (query) {
+      fetchSearchResults()
+    }
+    return () => {
+      controller.abort();
+      ignore = true;
+    }
+  }, [ query ]);
 
   return (
     <div>
@@ -15,6 +57,14 @@ function Search({ query }) {
         <button type="submit">Search</button>
       </form>
       <h2>Search query: {query}</h2>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <ul>
+          {repos.map(repo => <li key={repo.id}>{repo.full_name}</li>)}
+        </ul>
+      )}
+      {error && <ErrorContainer>Error!</ErrorContainer>}
     </div>
   );
 }
